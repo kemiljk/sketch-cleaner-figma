6ff5cbbdee42a57525c6831fb88f433cf2f5a7ae
Karl Koch <karl@kejk.tech>
2021-11-19 15:09:21 +0000

    fix: making the plugin run on a single command

--- code.js
+++ code.js
@@ -1,45 +1,26 @@
-figma.showUI(__html__);
-// function isRectangle(node: BaseNode): node is RectangleNode {
-//   return node.type === "RECTANGLE";
-// }
-// function isVector(node: BaseNode): node is VectorNode {
-//   return node.type === "VECTOR";
-// }
-// function isEllipse(node: BaseNode): node is EllipseNode {
-//   return node.type === "ELLIPSE";
-// }
+const { selection } = figma.currentPage;
 function clone(val) {
     return JSON.parse(JSON.stringify(val));
 }
-const { selection } = figma.currentPage;
-function removeMasks() {
+async function removeMasks() {
     selection.length === 0 && figma.notify("Nothing selected!");
     selection.forEach((node) => {
         node.isMask && node.remove();
         for (const child of node.children) {
-            child.isMask === false && figma.notify("No masks present");
-            child.isMask && child.remove();
+            child.isMask === true && child.remove();
         }
-        node.isMask === false && figma.notify("No masks present");
+        figma.notify("✅ Masks removed!");
     });
-    figma.notify("✅ Done!");
 }
-function removeStrokes() {
+async function removeStrokes() {
     selection.forEach((node) => {
         const strokes = clone(node.strokes);
         strokes[0].visible === false && strokes.splice(0, 1);
         node.strokes = strokes;
-        if (node.children) {
-            for (const child of node.children) {
-                const childStrokes = clone(child.strokes);
-                childStrokes[0].visible === false && childStrokes.splice(0, 1);
-                child.strokes = childStrokes;
-            }
-        }
+        figma.notify("✅ Strokes deleted!");
     });
-    figma.notify("✅ Done!");
 }
-function removeFills() {
+async function fixFills() {
     selection.forEach((node) => {
         node.fills = node.children[0].fills;
         node.strokes = node.children[0].strokes;
@@ -53,25 +34,16 @@ function removeFills() {
             node.cornerRadius = node.children[0].cornerRadius;
         }
         node.children[0].remove();
+        figma.notify("✅ Frame filled and old rectangle background removed!");
     });
-    figma.notify("✅ Done!");
 }
-figma.ui.onmessage = (msg) => {
-    if (msg.type === "rm-masks") {
-        removeMasks();
-    }
-    if (msg.type === "rm-strokes") {
-        removeStrokes();
-    }
-    if (msg.type === "fill-frame") {
-        removeFills();
-    }
-    if (msg.type === "fix-all") {
-        async function fixAll() {
-            await removeMasks();
-            await removeStrokes();
-            await removeFills();
-        }
-        fixAll();
-    }
-};
+removeMasks()
+    .then(() => {
+    fixFills();
+})
+    .then(() => {
+    removeStrokes();
+})
+    .finally(() => {
+    figma.closePlugin("✅ All finished!");
+});
